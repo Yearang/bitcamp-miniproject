81c7794cf2cea1a4741688622eb02fbf06ced663
jkj <dyttl@gmail.com>
2019-05-24 12:46:55 +0900

    'skillpage.js'에 아이콘 애니메이션과 그래프 퍼센트표시를 추가했습니다.

--- js/skillpage.js
+++ js/skillpage.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var width = 1;
     var id = setInterval(frame1, 20);
+
     function frame1() {
         if (width >= 85) {
             clearInterval($('#graph1'))
@@ -10,6 +11,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame2, 20);
+
     function frame2() {
         if (width >= 97) {
             clearInterval($('#graph2'))
@@ -19,6 +21,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame3, 20);
+
     function frame3() {
         if (width >= 95) {
             clearInterval($('#graph3'))
@@ -28,6 +31,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame4, 20);
+
     function frame4() {
         if (width >= 85) {
             clearInterval($('#graph4'))
@@ -37,6 +41,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame5, 20);
+
     function frame5() {
         if (width >= 90) {
             clearInterval($('#graph5'))
@@ -46,6 +51,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame6, 20);
+
     function frame6() {
         if (width >= 80) {
             clearInterval($('#graph6'))
@@ -55,6 +61,7 @@ $(document).ready(function() {
         }
     }
     var id = setInterval(frame7, 20);
+
     function frame7() {
         if (width >= 80) {
             clearInterval($('#graph7'))
@@ -63,5 +70,56 @@ $(document).ready(function() {
             $('#graph7').css('width', width + '%');
         }
     }
+    //%표시
+    var per = [];
+    for (let i = 0; i < $('.progress-bar').length; i++) {
+        if (i == 0) {
+            per.push(85);
+        } else if (i == 1) {
+            per.push(97);
+        } else if (i == 2) {
+            per.push(95);
+        } else if (i == 3) {
+            per.push(85);
+        } else if (i == 4) {
+            per.push(90);
+        } else if (i == 5) {
+            per.push(80);
+        } else if (i == 6) {
+            per.push(80);
+        }
+        $('.progress-bar').eq(i).on('mouseover', function () {
+            $('.progress-bar').eq(i).append(per[i]);
+            $('.progress-bar').eq(i).mouseleave(function () {
+                var test = $('.progress-bar').eq(i).text('');
+            });
+        });
+    }
+
+    //skill icon animation
+    $('img').on('mouseover', function () {
+        if ($('img').width() >= 140) {
+            $(this).animate({
+                width: '140px',
+                height: '140px'
+            }, 300, function () {
+                $(this).animate({
+                    width: '150px',
+                    height: '150px'
+                });
+            });
+        } else {
+            $(this).animate({
+                width: '110px',
+                height: '110px'
+            }, 300, function () {
+                $(this).animate({
+                    width: '150px',
+                    height: '150px'
+                });
+            });
+
+        }
+    });
 
 });
