811fa3b93c5f5f028680e72cb8f50411922633cf
yrko <book7080@naver.com>
2019-05-25 22:36:05 +0900

    terminal 추가

--- js/menu_mobile.js
+++ js/menu_mobile.js
@@ -1,21 +1,21 @@
-$('document').ready(function () {
-    $('.navbar-toggler').on('click',function(){
-        $('.nav-item').show();
-
-//        console.log('aaa');
-    });
-
-//    $(function () {
-//        $(".zeta-menu li").hover(function () {
-//            $('ul:first', this).show();
-//        }, function () {
-//            $('ul:first', this).hide();
-//        });
-//        $(".zeta-menu>li:has(ul)>a").each(function () {
-//            $(this).html($(this).html() + ' &or;');
-//        });
-//        $(".zeta-menu ul li:has(ul)")
-//            .find("a:first")
-//            .append("<p style='float:right;margin:-3px'>&#9656;</p>");
+//$('document').ready(function () {
+//    $('.navbar-toggler').on('click',function(){
+//        $('.nav-item').show();
+//
+////        console.log('aaa');
 //    });
-});
+//
+////    $(function () {
+////        $(".zeta-menu li").hover(function () {
+////            $('ul:first', this).show();
+////        }, function () {
+////            $('ul:first', this).hide();
+////        });
+////        $(".zeta-menu>li:has(ul)>a").each(function () {
+////            $(this).html($(this).html() + ' &or;');
+////        });
+////        $(".zeta-menu ul li:has(ul)")
+////            .find("a:first")
+////            .append("<p style='float:right;margin:-3px'>&#9656;</p>");
+////    });
+//});
---
+++ js/terminal.js
@@ -0,0 +1,55 @@
+function a() {
+    $('.command:last').after('<div class="command">');
+    $('.command:last').append('<b>koyearang@kyr-com</b>:~$ ').append('<input type="text" class="ipnut">');
+}
+$(document).ready(function () {
+//    $("#modal_show").click(function () {
+//        $("#terminalModal").modal("show");
+//    });
+    $('#terminalModal').on('shown.bs.modal', function () {
+        $('.modal-body').append('<div class="command">');
+        $('.command:last').append('<b>koyearang@kyr-com</b>:~$ ').append('<input type="text" class="ipnut">');
+        $('input').focus();
+    });
+
+    $("#close_modal").click(function () {
+        $("#terminalModal").modal("hide");
+    });
+
+    $('#terminalModal').on('hidden.bs.modal', function (e) {
+        console.log('modal close');
+        //        $(this).find('form')[0].reset()
+        $('.modal-body').empty();
+        a();
+    });
+    $('.modal-body').on('click', function () {
+        $('input').focus();
+    });
+    $('.modal-body').on('keydown', 'input', (function (key) {
+        if (key.keyCode == 13) {
+            console.log('aa');
+            var input = $(this).val();
+            $('.command:last').append(input);
+            if (input == 'help') {
+                $('.command:last').append("<p>yearang's terminal, version 1.0.0 <br>")
+                    .append('help - show all commands<br>')
+                    .append('whoami - show information about yearang<br>')
+                    .append('date - show current time<br>')
+                    .append('exit - close terminal</p>');
+
+            } else if (input == 'whoami') {
+                $('.command:last').append("<p>I'm yearang!</p>");
+            } else if (input == 'date') {
+
+            } else if (input == 'exit') {
+                $('#terminalModal').modal('hide');
+                return false;
+            } else {
+                $('.command:last').append('<p>' + input + ': command not found</p>');
+            }
+            $(this).remove();
+            a();
+            $('input').focus();
+        }
+    }));
+});
